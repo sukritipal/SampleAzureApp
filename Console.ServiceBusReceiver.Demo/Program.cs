f28dc4db6fafda586a934ef22a2e53176548fe52
Sukriti Pal <sukritipal6@gmail.com>
2023-12-14 01:46:07 +0530

    changes for Azure Service Bus

---
+++ Console.ServiceBusReceiver.Demo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://spsamplesb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=i0hvLtbdTSCv116CFj/vh4PqaB0RvfdJh+ASbJewVao=";
+const string queueOrTopicName = "spsamplequeue";
+const string subName = "sub1";
+
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine(body);
+    await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(serviceBusConnectionString);
+processor = client.CreateProcessor(queueOrTopicName, subName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("\nPress any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("\nStopped receiving messages");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+    throw;
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ Console.ServiceBusSender.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://spsamplesb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=i0hvLtbdTSCv116CFj/vh4PqaB0RvfdJh+ASbJewVao=";
+const string queueOrTopicName = "spsamplequeue";
+const int maxNumberOfMessages = 3;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(serviceBusConnectionString);
+sender = client.CreateSender(queueOrTopicName);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+for(int i = 1; i <= maxNumberOfMessages; i++)
+{
+    if (!batch.TryAddMessage(new ServiceBusMessage($"This is a message-{i}")))
+    {
+        Console.WriteLine($"Message-{i} was not added to the batch");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(batch);
+    Console.WriteLine("Messages sent");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+    throw;
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
